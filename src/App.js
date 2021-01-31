260cf5c174beac88167abc28bf0518e4689e21a8
Aniket <aam257@njit.edu>
2021-01-30 23:13:14 -0500

    redux shopping cart trial

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+// components
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+// items
+import cartItems from "./cart-items";
+// redux stuff
+import { createStore } from "redux";
+import reducer from "../src/reducer";
+import { Provider } from "react-redux";
+
+// initial store
+const initialStore = {
+  cart: cartItems,
+  total: 13,
+  amount: 5,
+};
+
+const store = createStore(reducer, initialStore);
+
+function App() {
+  // cart setup
+
+  return (
+    <Provider store={store}>
+      <Navbar />
+      <CartContainer />
+    </Provider>
+  );
+}
+
+export default App;
